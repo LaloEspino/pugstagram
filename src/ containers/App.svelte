5fbb70ec26f05ba54d33795a921e0dd781fffb07
laloespino <Ee45Q9Q4-github>
2020-04-18 21:03:14 -0500

    gitignore test

---
+++ src/ containers/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    let title = 'Pugstagram'
+</script>
+
+<style>
+    :global(body) {
+        background-color: #676778;
+        color: white;
+    }
+</style>
+
+<div class="main">
+    <h1>Hello {title}</h1>
+</div>
