c666c384d80287386a99663069366828ad4a4aa6
laloespino <Ee45Q9Q4-github>
2020-04-21 17:21:23 -0500

    Se agregan los componentes del proyecto

---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="" width="50px" height="50px"/>
+        </div>
+        <div class="Profile-info">
+            <h2>gndx</h2>
+            <span>Oscar Barajas</span>
+        </div>
+    </div>
+</div>
